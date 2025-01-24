e53046595d41f05719b471eb1e444faec90801c2
Felix <felix@weglehner.info>
2025-01-24 11:24:39 +0100

    Created home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,62 @@
 
 function App() {
 
+  const copyAddress = () => {
+    navigator.clipboard.writeText("nostalgicraft.com");
+  }
+
   return (
-    <></>
+    <div id="app">
+      <section id="title-container">
+        <div className='container-img' id="title-container-img"></div>
+        <h1 id="title" className="fantasy">Nostalgicraft</h1>
+        <a id="scroll-down-btn" href="#story"></a>
+      </section>
+      <section className="content-section" id="story">
+        <div className="section-content">
+          <h2>Welcome, wanderers!</h2>
+          <p>
+            Welcome, to the world of <b>Nostalgicraft</b>. <br />
+            Welcome, to a place where creativity flourishes and a world full of possibilities emerges.
+            Each new region of the vast world you travel feels like a marvelous dream that never ends.
+            Every block holds potential. Every resource is a tool for creation. From the tallest mountains to the deepest caves, everything in this world is yours to shape.
+            What are you going to build? What marks will you leave behind that make the sprouting land even more admirable than anyone would have thought?<br />
+            But be careful of the dark. As calm as the world looks, things change when the protecting sun sets and the hazardous night draws in.
+            Monsters will lurk in the shadows casted by the moon no matter how safe you feel and you should strive hard to survive.
+            Come and experience this adventure with your own eys, be creative, bring your friends, have fun!<br />
+            Yours sincerely<br />
+            <b>Felix</b>
+          </p>
+        </div>
+      </section>
+      <section id="address-container">
+        <div className='container-img' id="address-container-img"></div>
+        <button onClick={copyAddress} id="address" className="fantasy">nostalgicraft.com</button>
+      </section>
+      <section className="content-section">
+        <div className="section-content">
+          <h2>Technical</h2>
+          <p>You can join the server without mods at any time</p>
+          <p>However, I've created a Quality-of-Life Modpack for enhancing your immersive gameplay :)</p>
+        </div>
+      </section>
+      <section style={{"minHeight": "200px", "position": "relative"}}>
+        <div className='container-img' id="spacing-img"></div>
+      </section>
+      <section className="content-section">
+        <div className="section-content">
+          <h2>Rules</h2>
+          <ol>
+            <li>Don't grieve other buildings</li>
+            <li>Don't kill others intentionally</li>
+            <li>Don't be meanful</li>
+            <li>Don't scream around</li>
+            <li>Be creative. Nostalgicraft is more than just survival. The first goal is NOT having full enchanted armor!</li>
+            <li>Have fun!</li>
+          </ol>
+        </div>
+      </section>
+    </div>
   );
 }
 
